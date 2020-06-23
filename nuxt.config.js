0fbcca4cee80d309a77149b1c77b04054df5eab0
erison <erison@flexpeak.com.br>
2020-06-22 20:41:09 -0400

    removendo pusher

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,6 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '~/plugins/vue-pusher',
     '~/plugins/firebase.client',
   ],
   /*
--- plugins/vue-pusher.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import VuePusher from 'vue-pusher'
-Vue.use(VuePusher, {
-  api_key: process.env.PUSHER_API_KEY,
-  options: {
-    cluster: 'us2',
-    encrypted: true,
-  }
-})
